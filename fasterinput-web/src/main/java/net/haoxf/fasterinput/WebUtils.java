01da8e479fe9212b25ae12feb1d0b1ee99ef955e
Wangpeng <kikoroc@gmail.com>
2015-10-29 18:42:11 +0800

    daily

---
+++ fasterinput-web/src/main/java/net/haoxf/fasterinput/WebUtils.java
@@ -0,0 +1,21 @@
+package net.haoxf.fasterinput;
+
+import com.google.gson.Gson;
+import net.haoxf.fasterinput.model.HttpRet;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * Created by Administrator on 2015/10/29 0029.
+ */
+public class WebUtils {
+    private static Gson gson = new Gson();
+
+    public static void writeHttpRetToResponse(HttpServletResponse response, HttpRet ret) throws IOException{
+        response.setContentType("application/json;charset=UTF-8");
+        response.getWriter()
+                .write(gson.toJson(ret));
+        response.getWriter().flush();
+    }
+}
